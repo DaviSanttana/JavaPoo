94c591fe47d53de9c044f3c0f42d75a32ea2de36
Davi <batatovisk0330@gmail.com>
2026-01-30 14:54:28 -0300

    Classes poo - atributos

---
+++ src/TesteAtributos.java
@@ -0,0 +1,15 @@
+public class TesteAtributos {
+    public static void main(String[] args) {
+        int idade = 19;
+        double altura = 1.74;
+        float alturaf = 1.74;
+        char sexo = 'M';
+        String nome = "Davi Gabriel";
+        boolean ligado = true;
+
+        Integer idadeObj = 20;
+        Double alturaObj = 1.75;
+        Boolean ligadoObj = true;
+
+    }
+}
